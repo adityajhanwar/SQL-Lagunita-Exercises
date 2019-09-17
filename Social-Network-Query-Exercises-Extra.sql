9305110070c38f146e6adfd7e42789b933ef7a9a
adityajhanwar <ajhanwar@berkeley.edu>
2019-09-17 16:38:22 -0700

    init new exercises file

---
+++ Social-Network-Query-Exercises-Extra.sql
@@ -0,0 +1,26 @@
+/*
+ * Social-Network-Query-Exercises.sql
+ * 9/16/19
+ * Aditya Jhanwar
+ * My solutions for Stanford Lagunita's SQL Database Course, exercise set 'SQL Social-Network Query Exercises'
+ */
+
+-- 1. For every situation where student A likes student B, but student B likes a different student C, return the names and grades of A, B, and C.
+
+...
+
+-- 2. Find those students for whom all of their friends are in different grades from themselves. Return the students' names and grades.
+ 
+...
+
+-- 3. What is the average number of friends per student? (Your result should be just one number.)
+ 
+...
+
+-- 4. Find the number of students who are either friends with Cassandra or are friends of friends of Cassandra. Do not count Cassandra, even though technically she is a friend of a friend.
+
+...
+
+-- 5. Find the name and grade of the student(s) with the greatest number of friends.
+
+...
